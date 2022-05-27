a62fb628e11882b639b31faf956c5faaccce16be
R a b i a <rabia.saeed.1995@gmail.com>
2022-05-27 19:00:53 -0400

    working on local storage saving

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # daily-planner
+
+//https://stackoverflow.com/questions/13227360/javascript-attach-events-in-loop
+
--- scripts.js
+++ scripts.js
@@ -3,34 +3,62 @@ currentDay = moment();
 let today = currentDay.format("MMMM Do YYYY");
 $("#currentDay").text(today);
 
+let hour = currentDay.format("H HH");
+
+$(document).on("click", ".save-btn", function () {
+	const id = $(this).attr("id");
+	const index = id.split("--")[1];
+	const task = $("#task--" + index).val();
+
+	if (task !== "") {
+		localStorage.setItem(`${moment().format("MMMM Do YYYY")}--${index}`, task);
+	}
+});
+
 const tableContent = $("#tableContent");
 
 for (i = 0; i < 24; i++) {
-	//     <tr>
-	//   <th scope="row">3</th>
-	//   <td colspan="2" class="table-active">
-	//     <input type="text" class="form-control" placeholder="something">
-	//   </td>
-	//   <td>save</td>
-	// </tr>
-
-	// create the virtual dom nodes and assign it to consts after calling jquery on it
-
 	const tableRow = $(document.createElement("tr"));
 	const tableHours = $(document.createElement("th"));
 	const inputColumn = $(document.createElement("td"));
 	const taskInput = $(document.createElement("input"));
 	const saveColumn = $(document.createElement("td"));
+	const saveButton = $(document.createElement("btn"));
+	const saveIcon = $(document.createElement("i"));
+
+	let existingTask = localStorage.getItem(
+		`${moment().format("MMMM Do YYYY")}--${i}`
+	);
 
 	// append table hours to the table row with the hour
 	tableHours.text(i);
+	tableHours.prop({ scope: "row" });
 	tableHours.appendTo(tableRow);
 
 	// create the input column
+	inputColumn.prop({ colspan: 2 });
+	inputColumn.addClass("table-active");
+	taskInput.addClass("form-control");
+	taskInput.attr({ id: "task--" + i });
 	taskInput.appendTo(inputColumn);
+
+	if (existingTask !== null) {
+		taskInput.val(existingTask);
+	}
+
 	inputColumn.appendTo(tableRow);
 
+	// creating save column
+	saveColumn.prop({ colspan: 1 });
+
+	saveButton.text("Save ");
+	saveButton.addClass("btn btn-primary save-btn");
+	saveButton.prop({ id: `save--${i}` });
+	saveIcon.addClass("bi bi-save");
+	saveIcon.appendTo(saveButton);
+	saveButton.appendTo(saveColumn);
 	saveColumn.appendTo(tableRow);
 
+	//appending table row to table content
 	tableRow.appendTo(tableContent);
 }
