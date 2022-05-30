2a8185c4adac580024cf0749cec0c7f16b99d639
R a b i a <rabia.saeed.1995@gmail.com>
2022-05-29 21:47:35 -0400

    added hourUpdater function to handle hour changes

--- scripts.js
+++ scripts.js
@@ -3,14 +3,13 @@ currentDay = moment();
 let today = currentDay.format("MMMM Do YYYY");
 $("#currentDay").text(today);
 
-const businessHours = currentDay.format("hh:mm A");
-
 // in 24 hour clock -> when do you start working?
 const startOfWorkDayHour = 9;
 
 // how many hours do you want to work
 const hoursToWork = 9;
 
+// when the node with the class save-btn is clicked,
 $(document).on("click", ".save-btn", function () {
 	const id = $(this).attr("id");
 	// splitting by -- to set an index
@@ -22,45 +21,55 @@ $(document).on("click", ".save-btn", function () {
 
 const tableContent = $("#tableContent");
 
-// workday to be from 9am -6pm
+//  creates the table rows for each hour of the day
 for (i = 0; i < hoursToWork; i++) {
-	const tableRow = $(document.createElement("tr"));
-	const tableHours = $(document.createElement("th"));
-	const inputColumn = $(document.createElement("td"));
-	const taskInput = $(document.createElement("textarea"));
-	const saveColumn = $(document.createElement("td"));
-	const saveButton = $(document.createElement("btn"));
-	const saveIcon = $(document.createElement("i"));
+	// create all the required dom nodes
+	const tableRow = $("<tr>");
+	const tableHours = $("<th>");
+	const inputColumn = $("<td>");
+	const taskInput = $("<textarea>");
+	const saveColumn = $("<td>");
+	const saveButton = $("<button>");
+	const saveIcon = $("<i>");
 
-	const workHour = moment().set("hour", i + startOfWorkDayHour);
-	const currentHour = moment().startOf("hour");
-
+	// see if the task for given hour
+	// already exists in the local storage of the user
 	let existingTask = localStorage.getItem(
 		`${currentDay.format("MMMM-Do-YYYY")}--${i}`
 	);
 
-	// append table hours to the table row with the hour
+	// set  the id to the hour so we can compare the hour of the table row
+	// with the current hour in the hour updater function
+	tableRow.prop({ id: i + startOfWorkDayHour });
+	tableRow.addClass("hour-row");
+
+	const workHour = moment({ hour: i + startOfWorkDayHour });
 	tableHours.text(workHour.format("h:00 A"));
+
 	tableHours.prop({ scope: "row" });
+	tableHours.addClass("table-hour");
 	tableHours.appendTo(tableRow);
 
 	// create the input column
 	inputColumn.prop({ colspan: 2 });
-	inputColumn.addClass("table-active");
+	inputColumn.addClass("table-active").addClass("table-input-col");
 	taskInput.addClass("form-control");
 	taskInput.attr({ id: "task--" + i, rows: 3 });
 
 	taskInput.appendTo(inputColumn);
 
+	// if there is a task from local storage, set the value of the input
+	// to the task that is persisted in the local storage
+
 	if (existingTask !== null) {
 		taskInput.val(existingTask);
 	}
 
+	// append the nodes to their parents
 	inputColumn.appendTo(tableRow);
 
-	// creating save column
+	// saveButton and saveColumn nodes
 	saveColumn.prop({ colspan: 1 });
-
 	saveButton.text("Save ");
 	saveButton.addClass("btn btn-primary save-btn");
 	saveButton.prop({ id: `save--${i}` });
@@ -69,23 +78,36 @@ for (i = 0; i < hoursToWork; i++) {
 	saveButton.appendTo(saveColumn);
 	saveColumn.appendTo(tableRow);
 
-	// if the current hour is set to the work hour
-	// then the input column color will be teal (table-info)
-	if (workHour.format("hh a") == currentHour.format("hh a")) {
-		inputColumn.addClass("table-info");
-		taskInput.attr({ placeholder: "Current hour" });
-	}
-	if (workHour.isBefore(currentDay, "hour")) {
-		saveButton.addClass("disabled");
-		taskInput.prop({ readonly: true });
-		taskInput.attr({ placeholder: "Past" });
-	}
-
-	if (workHour.isAfter(currentDay, "hour")) {
-		inputColumn.addClass("table-success");
-		taskInput.attr({ placeholder: "Add task" });
-	}
-
 	//appending table row to table content
 	tableRow.appendTo(tableContent);
 }
+
+function hourUpdater() {
+	// function to rest classes for past , current and future hours in the table
+	// selects all elements with the class .hour-row and loops through them to check if the
+	// hour has past by checking the value in the id with the current hour from moment
+	$(".hour-row").each(function () {
+		const hour = $(this).attr("id");
+		// if the current hour is set to the work hour
+		// then the input column color will be teal (table-info)
+
+		if (hour < moment().hours()) {
+			$(this).find("textarea").attr({ placeholder: "Past", readonly: true });
+			$(this).find("button").addClass("disabled");
+		}
+
+		if (hour == moment().hours()) {
+			$(this).find(".table-input-col").addClass("table-info");
+			$(this).find("textarea").attr({ placeholder: "Current hour" });
+		}
+
+		if (hour > moment().hours()) {
+			$(this).find(".table-input-col").addClass("table-success");
+			$(this).find("textarea").attr({ placeholder: "Add task" });
+		}
+	});
+}
+hourUpdater();
+
+// re-runs hourUpdater function after every 30 seconds
+setInterval(hourUpdater, 30000);
